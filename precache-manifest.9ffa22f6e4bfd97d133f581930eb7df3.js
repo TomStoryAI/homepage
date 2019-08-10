e68ec36ad1fdc968808592a7d9eb257cc7104a93
Nowicki <Thomas.Nowicki@viadee.de>
2019-08-10 19:46:14 +0200

    Updates

---
+++ precache-manifest.9ffa22f6e4bfd97d133f581930eb7df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29e7f85f32dcc5a618d36da3e9dc4ea",
+    "url": "/homepage/frontend/index.html"
+  },
+  {
+    "revision": "f1dbf6015da75f02dc33",
+    "url": "/homepage/frontend/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "427cfbcfdc6b7dd9dd2f",
+    "url": "/homepage/frontend/static/css/main.189cbdfc.chunk.css"
+  },
+  {
+    "revision": "f1dbf6015da75f02dc33",
+    "url": "/homepage/frontend/static/js/2.9b39eba0.chunk.js"
+  },
+  {
+    "revision": "427cfbcfdc6b7dd9dd2f",
+    "url": "/homepage/frontend/static/js/main.037e497f.chunk.js"
+  },
+  {
+    "revision": "b9be1e159b0c05be1f82",
+    "url": "/homepage/frontend/static/js/runtime~main.c3ed7810.js"
+  },
+  {
+    "revision": "6d98c71a9386414fff9bde843ef39684",
+    "url": "/homepage/frontend/static/media/thomas.6d98c71a.jpg"
+  }
+]);
